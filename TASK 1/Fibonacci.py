14ae28b64c3d7d06ee1b0c97d4422ac49632801f
Harsh Raj <officialharshrajcse@gmail.com>
2025-03-03 14:19:16 +0000

    Fibonacci numbers

---
+++ TASK 1/Fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci_memo(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        memo[n] = fibonacci_memo(n - 1, memo) + fibonacci_memo(n - 2, memo)
+        return memo[n]
+
+# Print first 100 Fibonacci numbers
+for i in range(100):
+    print(fibonacci_memo(i), end=" ")
